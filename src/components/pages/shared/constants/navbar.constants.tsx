865204cf7c0e49a4f216bbce60077b8cc829e71f
ArifMiah07 <arifmiah.me322@gmail.com>
2025-05-18 23:50:08 +0600

    making dynamic routes with icon

--- src/components/pages/shared/constants/navbar.constants.tsx
+++ src/components/pages/shared/constants/navbar.constants.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { Magnet, MessageCircle } from "lucide-react";
 import Link from "next/link";
-
+import { ReactNode } from "react";
+import { Bell } from 'lucide-react';
 // styles
 const stylesForLi: string =
   "flex third-layer-background w-8 h-8  items-center justify-center rounded-full border border-[#222FE9]";
@@ -8,6 +10,85 @@ const stylesForLiSpan: string = "heading-text-rgb";
 
 // contents
 const notifyLinksInnerContents: string[] = ["R", "M", "L", "N", "O"];
+
+
+type TNotifyRoutes = {
+  icon?: ReactNode;
+  label: string;
+  text: string;
+  route?: string;
+  altText?: string;
+  children?: ReactNode | null;
+};
+
+const notifyRoutes : TNotifyRoutes[] = [
+    {
+        icon: <Bell /> ,
+        label: 'Clyster',
+        text: 'Clyster',
+        route: 'clyster',
+        altText: 'clyster messenger app',
+        children: null,
+    },
+    {
+        icon: <Magnet />,
+        label: 'Profile',
+        text: 'Profile',
+        route: 'profile',
+        altText: 'users profile page',
+        children: null,
+    },
+    {
+        icon: <MessageCircle />,
+        label: 'Clyster',
+        text: 'Clyster',
+        route: 'clyster',
+        altText: 'clyster messenger app',
+        children: null,
+    },
+    {
+        icon: <Magnet />,
+        label: 'Profile',
+        text: 'Profile',
+        route: 'profile',
+        altText: 'users profile page',
+        children: null,
+    },
+];
+const menuRoutes : TNotifyRoutes[] = [
+    {
+        icon: <MessageCircle />,
+        label: 'Clyster',
+        text: 'Clyster',
+        route: 'clyster',
+        altText: 'clyster messenger app',
+        children: null,
+    },
+    {
+        icon: <Magnet />,
+        label: 'Profile',
+        text: 'Profile',
+        route: 'profile',
+        altText: 'users profile page',
+        children: null,
+    },
+    {
+        icon: <MessageCircle />,
+        label: 'Clyster',
+        text: 'Clyster',
+        route: 'clyster',
+        altText: 'clyster messenger app',
+        children: null,
+    },
+    {
+        icon: <Magnet />,
+        label: 'Profile',
+        text: 'Profile',
+        route: 'profile',
+        altText: 'users profile page',
+        children: null,
+    },
+];
 const navLinksInnerContent: string[] = [
   "R",
   "M",
@@ -26,13 +107,15 @@ const menuBarLinksInnerContent: string[] = ["C", "P", "D", "M", "O"];
 // notifyLinks
 export const notifyLinks = (
   <>
-    {notifyLinksInnerContents.map((innerContent: string, index: number) => (
-      <li key={index} className={`${stylesForLi}`}>
-        <Link href={`/${(innerContent.toLowerCase())}`}>
-          <span className={`${stylesForLiSpan}`}>{innerContent}</span>
-        </Link>
-      </li>
-    ))}
+    {
+        notifyRoutes.map((innerContent:TNotifyRoutes , index: number) => (
+        <li key={index} className={`${stylesForLi}`}>
+            <Link href={`/${innerContent.route}`}>
+            <span className={`${stylesForLiSpan}`}>{innerContent.icon}</span>
+            </Link>
+        </li>
+        ))
+    }
   </>
 );
 
@@ -52,12 +135,14 @@ export const navLinks = (
 // User Specific menu bar
 export const menuBarLinks = (
   <>
-    {menuBarLinksInnerContent.map((innerContent: string, index: number) => (
-      <li key={index} className={`${stylesForLi}`}>
-        <Link href={`/${(innerContent.toLowerCase())}`}>
-          <span className={`${stylesForLiSpan}`}>{innerContent}</span>
-        </Link>
-      </li>
-    ))}
+    {
+        menuRoutes.map((innerContent:TNotifyRoutes , index: number) => (
+        <li key={index} className={`${stylesForLi}`}>
+            <Link href={`/${innerContent.route}`}>
+            <span className={`${stylesForLiSpan}`}>{innerContent.icon}</span>
+            </Link>
+        </li>
+        ))
+    }
   </>
 );
